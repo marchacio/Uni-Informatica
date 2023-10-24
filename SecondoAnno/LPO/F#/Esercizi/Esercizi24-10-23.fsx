9fe5e8261307403c66148bb2d012198b20dc877b
Marco Zoratti <marco.zoratti.03@gmail.com>
2023-10-24 16:34:40 +0200

    Es 24/10/23

--- SecondoAnno/LPO/F#/Esercizi/Esercizi24-10-23.fsx
+++ SecondoAnno/LPO/F#/Esercizi/Esercizi24-10-23.fsx
@@ -1,4 +1,14 @@
 (*
+------------------------------------------------------------------------
+Per eseguire i file, installa dotnet sul pc poi, nella cartella contenente 
+i file .fsx (F Sharp eXecutive) chiama il comando:
+
+'dotnet fsi NOME_FILE.fsx'
+
+------------------------------------------------------------------------
+*)
+
+(*
     ES 1:
     Definire la funzione generica genSum : (int -> int) -> int -> int tale che genSum f n calcola f 0 + f 1 + ... + f n.
 
