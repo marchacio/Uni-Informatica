dd6316297d727c13681ca6a25e85d672b2419792
Marco Zoratti <marco.zoratti.03@gmail.com>
2023-11-14 16:06:26 +0100

    Labo1 ALAN

--- SecondoAnno/ALAN/labo1/es1.cpp
+++ SecondoAnno/ALAN/labo1/es1.cpp
@@ -16,16 +16,19 @@ const int LUNGH_MATR = 7;
 //-----------------------------------------------------------------------
 
 int fattoriale(int n){
-    if(n == 0) //caso base
-        return n;
+    int fattor = 1;
+    for (int i = 1; i <= n; i++)
+        fattor *= i;
     
-    return n*fattoriale(n-1);
+    return fattor;
 }
 
+//funzione esponenziale
 double f(double x) {
     return exp(x);
 }
 
+//funzione Polinomio di Taylor
 double fNx(double n, double x) {
     if(x == 0)
         throw "x uguale a 0, errore!";
@@ -48,40 +51,97 @@ void es1() {
     char s0 = nMatr[LUNGH_MATR-1];
     char s1 = nMatr[LUNGH_MATR-2];
 
+    //converti il char in se stesso ma numero
     int d0 = ((int)s0)-((int)'0');
-    int d1 = ((int)s0)-((int)'0');
+    int d1 = ((int)s1)-((int)'0');
+
+    cout << "Numero matricola: " << nMatr << "; ultime due cifre: " << d0 << ", " << d1 << ".\n";
     
+    //facendo i calcoli matematici, il risultato di a+b+c (indipendentemente dalle parentesi) dovrebbe essere a,
+    //in quanto c = -b e quindi c e b si semplificano.
+    //Come si vede dall'output del programma, il primo metodo di calcolo (a+b)+c è instabile, mentre il secondo è stabile
+    //e riporta i risultati corretti.
     for(int i = 0; i <= 6; ++i){
         double a = (double)(d0+1)*pow(10, i);
         double b = (double)(d1+1)*pow(10, 20);
         double c = -b;
 
-        cout << (a+b)+c << "   " << a+(b+c) << "\n";
+        cout << (a+b)+c << "\t" << a+(b+c) << "\n";
     }
 }
 
 void es2(double x1, double x2) {
 
-    cout << "Valori x in input: " << x1 << " " << x2 << ":\n";
+    cout << "Valori x in input: " << x1 << ", " << x2 << ":\n\n";
 
-    cout << "f: " <<  f(x1) << "   " << f(x2) << "\n";
+    double fx1 = f(x1);
+    double fx2 = f(x2);
+
+    cout << "output di f: " << fx1 << "   " << fx2 << "\n\n";
 
     const int SIZE = 5;
     int numeri[SIZE] = {3, 10, 50, 100, 150};
 
-    for(int i = 0; i < SIZE; ++i)
-        cout << "fN: " <<  fNx(numeri[i], x1) << "   " << fNx(numeri[i], x2) << "\n";
+    for(int i = 0; i < SIZE; ++i){
+        double fNx1 = fNx(numeri[i], x1);
+        double fNx2 = fNx(numeri[i], x2);
+        
+        //formula errore assoluto: EA = x('perturbata') - x
+        double errAssoluto1 = fNx1 - fx1;
+        double errAssoluto2 = fNx2 - fx2;
+
+        //formula errore relativo: ER = ( x('perturbata') - x ) / x
+        double errRelativo1 = (fNx1 - fx1)/fx1;
+        double errRelativo2 = (fNx2 - fx2)/fx2;
+
+        //il "reciproco di n" è quel numero che moltiplicato per n fa 1.
+        double reciproco1 = 1/fNx1;
+        double reciproco2 = 1/fNx2;
+
+        //stampa risultati ottenuti
+        cout << "fN(N = " << numeri[i] << "): " << fNx1 << " (EA: " << errAssoluto1 << ", ER: " << errRelativo1 << ", REC: " << reciproco1 << ")   " 
+        << fNx2 << " (EA: " << errAssoluto2 << ", ER: " << errRelativo2 << ", REC: " << reciproco2 << ").\n";
+    }    
+}
+
+/*
+Implementare un programma che determina la precisione di macchina eps, ossia il valore positivo eps =2^(−d), 
+dove d è il piu grande intero positivo tale che 1 + 2^(−d) > 1 in aritmetica di macchina; calcolarne il
+valore sia in singola che in doppia precisione.
+*/
+void es3(){
     
+    //singola precisione
+    float eps = 1;
+    while(eps+1 != 1) //ovvero fino a quando eps non risulta 0
+        eps /= 2;
+
+    cout << "Precisione di macchina (eps) in singola precisione: " << eps << "\n";
+
+    //doppia precisione
+    double eps2 = 1;
+    while(eps2+1 != 1) //ovvero fino a quando eps non risulta 0
+        eps2 /= 2;
+
+    cout << "Precisione di macchina (eps) in doppia precisione: " << eps2 << "\n";
 }
 
 
 int main() {
 
-    //es1();
+    cout << "\n--------------------------ESERCIZIO 1--------------------------\n";
+    es1();
+
+    cout << "\n\n--------------------------ESERCIZIO 2--------------------------\n";
 
     es2(0.5, 30);
     cout << "\n\n";
     es2(-0.5, -30);
+
+    cout << "\n\n--------------------------ESERCIZIO 3--------------------------\n";
+
+    es3();
+
 
     return 0;
 }
